62bfc67f9628bf93907029df45e10537c65ca3c0
Seray Ciftci <seri@Mac.fritz.box>
2025-07-02 16:51:45 +0200

    Added xcode project, asset folder  and test folders

---
+++ PinkCloudsApp/Pink_CloudsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pink_CloudsApp.swift
+//  Pink Clouds
+//
+//  Created by Seray Ciftci on 02.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Pink_CloudsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tests/Pink_CloudsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pink_CloudsTests.swift
+//  Pink CloudsTests
+//
+//  Created by Seray Ciftci on 02.07.25.
+//
+
+import Testing
+@testable import Pink_Clouds
+
+struct Pink_CloudsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
